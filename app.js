66d9be696a8ec2874b2240f60e833e6e6018db14
wwf <865475679@qq.com>
2020-03-09 19:39:01 +0800

    修改

---
+++ app.js
@@ -0,0 +1,9 @@
+App({
+  globalData: {
+    host: 'https://gp-server.hunger-valley.com',  //照抄
+    app_id: "wx15f3479cc1352b78 ",
+    app_secret: "aa002db81d2c9a81aae564485722e03f",
+    t_app_id: "6vSHR8CSuNZpr9HsC2J2qv8W",
+    t_app_secret: "M9TL1v37uAUeWWoCz7PDbCnD"
+  }
+})
