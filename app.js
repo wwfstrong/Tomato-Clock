1ec786a519b31230920038a84a012781ee184737
wwf <865475679@qq.com>
2020-03-10 09:03:30 +0800

    删除

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com', 
-    app_id: "wx15f3479cc1352b78 ",
-    app_secret: "77e6da5ad66e89cbcc3cd9d563b02e22",
-    t_app_id: "pd1cw6rbCh1EccK6stTzwo9D",
-    t_app_secret: "KGjA5sVLc8HyFrSop5qT8Lns"
-  }
-})
